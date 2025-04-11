85d6932c33f9b93706842a7bf078f7db4faad934
dubanteo19 <21130444@st.hcmuaf.edu.vn>
2025-04-11 15:02:27 +0700

    creat grid system for user layout

--- src/components/layout/UserLayout.tsx
+++ src/components/layout/UserLayout.tsx
@@ -1,7 +1,15 @@
+import { Outlet } from "react-router-dom";
+import { LeftSideBar } from "../admin/LeftSideBar";
+import { RightSideBar } from "../admin/RightSideBar";
 
 export const UserLayout = () => {
   return (
-    <div>UserLayout</div>
-  )
-}
-
+    <div className="grid grid-cols-12 h-screen px-16">
+      <LeftSideBar className="col-span-2 bg-green-500" />
+      <div className="col-span-7">
+        <Outlet />
+      </div>
+      <RightSideBar className="col-span-3 bg-orange-500" />
+    </div>
+  );
+};
